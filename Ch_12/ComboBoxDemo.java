d8c882d23746fe31c955a0c6e41001cb3fa1ebe3
InJoon-L <dlswns619@naver.com>
2021-03-11 12:43:43 +0900

    이미지 링크 수정안한 버전

---
+++ Ch_12/ComboBoxDemo.java
@@ -0,0 +1,46 @@
+package Ch_12;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ComboBoxDemo extends JFrame implements ActionListener{
+	private JLabel label;
+	private JComboBox animalList;
+	
+	public ComboBoxDemo() {
+		this.setTitle("ÄÞº¸ ¹Ú½º");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500, 400);
+		
+		String[] animals = { "dog", "lion", "tiger" };
+		animalList = new JComboBox(animals);
+		animalList.setSelectedIndex(0);
+		animalList.addActionListener(this);
+		
+		label = new JLabel();
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		changePicture(animals[animalList.getSelectedIndex()]);
+		
+		this.add(animalList, BorderLayout.NORTH);
+		this.add(label, BorderLayout.CENTER);
+		this.setVisible(true);
+	}
+	
+	protected void changePicture(String name) {
+		ImageIcon icon = new ImageIcon(name + ".gif");
+		label.setIcon(icon);
+		
+		label.setText(null);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String name = (String) animalList.getSelectedItem();
+		changePicture(name);
+	}
+	
+	public static void main(String[] args) {
+		new ComboBoxDemo();
+	}
+}
--- Ch_12/TextAreaDemo.java
+++ Ch_12/TextAreaDemo.java
@@ -13,14 +13,32 @@ public class TextAreaDemo extends JFrame implements ActionListener{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		textField = new JTextField(30);
+		textField.addActionListener(this);
+		
+		textArea = new JTextArea(10, 30);
+		textArea.setEditable(false);
+		
+		this.add(textField, BorderLayout.NORTH);
+		
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		
+		this.add(scrollPane, BorderLayout.CENTER);
+		
+		this.pack();
+		this.setVisible(true);
 	}
 	
 	public static void main(String[] args) {
-		
+		new TextAreaDemo();
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		String text = textField.getText();
+		textArea.append(text + "\n");
+		
+		textField.selectAll();
+		textArea.setCaretPosition(textArea.getDocument().getLength());
 		
 	}
 	
