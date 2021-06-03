0a099addb27e84ef3bc813a959043d1a47bed087
InJoon-L <dlswns619@naver.com>
2021-06-03 16:49:37 +0900

    setting gson

--- Ch_18/HttpURLPostTest.java
+++ Ch_18/HttpURLPostTest.java
@@ -5,18 +5,20 @@ import java.io.*;
 
 public class HttpURLPostTest {
 	public static void main(String[] args) {
-		String site = "http://localhost:8080/test";
+		String site = "http://localhost:9090/todos";
 
 		try {
 			URL url = new URL(site);
 			
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			
+			con.setDoInput(true);
 			con.setDoOutput(true);
 			con.setRequestMethod("POST");
 			con.setRequestProperty("content-type", "application/x-www-form-urlencoded");
 			
 			// id=scpark
+			String data = "id=scpark&pw=1111";
 			
 			OutputStream stream = con.getOutputStream();
 			
@@ -24,7 +26,22 @@ public class HttpURLPostTest {
 			
 			PrintWriter writer = new PrintWriter(owriter);
 			
-			writer.println("id=scpark&pw=1111");
+			writer.println(data);
+			writer.flush();
+			
+			InputStream istream = con.getInputStream();
+			
+			InputStreamReader ireader = new InputStreamReader(istream);
+			
+			BufferedReader reader = new BufferedReader(ireader);
+		    String line;
+		    
+		    while((line = reader.readLine()) != null) {
+		       System.out.println(line);
+		    }
+		    
+		    System.out.println();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
---
+++ Ch_18/PostJson.java
@@ -0,0 +1,21 @@
+package Ch_18;
+
+import java.net.*;
+import java.io.*;
+import com.google.gson.*;
+
+public class PostJson {
+	public static void main(String[] args) {
+		Gson gson = new Gson();
+		
+		String site = "https://jsonplaceholder.typicode.com/posts";
+		
+		// 연결
+		// 결과를 입력스트립으로 받아서 문자열로 저장.
+		// 저장된 문자열로부터 자바 객체 생성
+		
+		// 자바 객체를 이용해서 여러가지 처리를 할 수 있겠지..
+		// 예를 들면, DB에 저장할 수도 있고...
+		
+	}
+}
